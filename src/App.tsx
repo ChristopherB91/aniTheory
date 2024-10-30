52037ad43e2d66db3058543d3a73603538c99b6d
Chris <chrisbrowne91@gmail.com>
2024-10-30 12:29:19 -0400

    changed styling location

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,31 @@
 import { Homepage } from "./pages/homepage";
 import { Navbar } from "./components/navbar";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { createBrowserRouter, RouterProvider, Outlet } from "react-router-dom";
 import { Posts } from "./pages/post";
 import { ErrorPage } from "./pages/errorpage";
+
+const Layout = () => (
+  <>
+    <Navbar />
+    <Outlet />
+  </>
+);
 
 function App() {
   const router = createBrowserRouter([
     {
-      path: "/",
-      element: <Homepage />,
-      errorElement: <ErrorPage />,
-    },
-    {
-      path: "/post",
-      element: <Posts />,
+      element: <Layout />,
+      children: [
+        { path: "/", element: <Homepage /> },
+        { path: "/posts", element: <Posts /> },
+      ],
       errorElement: <ErrorPage />,
     },
   ]);
 
   return (
     <>
-      <Navbar />
-      <div className="flex justify-center items-center flex-grow">
+      <div className="m-0 p-0 h-full box-border flex flex-col">
         <RouterProvider router={router} />
       </div>
     </>
--- src/pages/homepage.tsx
+++ src/pages/homepage.tsx
@@ -2,8 +2,8 @@ import welcomeD from "../assets/welcomeDark.svg";
 
 export const Homepage = () => {
   return (
-    <>
+    <div className="flex justify-center items-center flex-grow">
       <img src={welcomeD} alt="Welcome" />
-    </>
+    </div>
   );
 };
