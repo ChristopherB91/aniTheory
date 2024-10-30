2198acb0ab354c2fabf100e9f8e63dc3f821a867
Chris <chrisbrowne91@gmail.com>
2024-10-30 12:28:26 -0400

    navbar functional

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,20 +1,23 @@
 import logo from "../assets/logo.svg";
+import { Link } from "react-router-dom";
 
-export const Navbar = () => {
+export const Navbar: React.FC = () => {
   return (
     <>
-      <nav className="border-b-4 border-solid border-b-red-600 flex justify-between items-center font-custom">
-        <img
-          src={logo}
-          alt="logo"
-          className="h-16 max-w-ful overflow-visible"
-        />
-        <ul className="list-none text-red-600 text-3xl">
-          <b className="flex gap-2">
-            <li>Post</li>
-            <li>Favorites</li>
-            <li>Contact</li>
-          </b>
+      <nav className="border-b-4 border-solid border-b-red-600 flex flex-wrap justify-center items-center font-custom sm:justify-around md:justify-between">
+        <Link to="/">
+          <img
+            src={logo}
+            alt="logo"
+            className="h-16 max-w-ful overflow-visible"
+          />
+        </Link>
+        <ul className="list-none font-bold text-red-600 text-3xl flex flex-wrap gap-2 justify-center">
+          <li>
+            <Link to="posts">Post</Link>
+          </li>
+          <li>Favorites</li>
+          <li>Contact</li>
         </ul>
       </nav>
     </>
