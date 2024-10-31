d21b65eb1293b404f74b3d705ba010aa7950ba2a
Chris <chrisbrowne91@gmail.com>
2024-10-31 12:40:27 -0400

    added a return button from error page

--- src/pages/errorpage.tsx
+++ src/pages/errorpage.tsx
@@ -1,4 +1,4 @@
-import { useRouteError } from "react-router-dom";
+import { Link, useRouteError } from "react-router-dom";
 
 interface RouteError {
   message: string;
@@ -16,6 +16,9 @@ export const ErrorPage: React.FC = () => {
       <p>
         <i>{error.status || error.message}</i>
       </p>
+      <button className="p-4 border-solid border-red-600 border-2 rounded-2xl">
+        <Link to="/">Click here to return</Link>
+      </button>
     </div>
   );
 };
