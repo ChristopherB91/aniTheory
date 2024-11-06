52b27bba0680d13b555fb7b057862d79964aa6dc
Chris <chrisbrowne91@gmail.com>
2024-11-06 14:14:47 -0500

    fixed contact form

--- src/components/contact.tsx
+++ src/components/contact.tsx
@@ -13,7 +13,7 @@ const ContactForm = () => {
     emailjs
       .sendForm(
         "service_myd2gev",
-        "template_so5nw5o",
+        "template_knlovc1",
         form,
         "user_NPqoDtEviZiRb1SzduPZ5"
       )
@@ -25,40 +25,46 @@ const ContactForm = () => {
           setTimeout(() => {
             setStateMessage(null);
           }, 5000);
+
+          form.reset();
         },
         (error) => {
-          setStateMessage(error);
+          setStateMessage(error.text || "An error occurred, please try again.");
           setIsSubmitting(false);
           setTimeout(() => {
             setStateMessage(null);
           }, 5000);
         }
       );
-
-    // Clears the form after sending the email
-    form.reset();
   };
   return (
     <form
       onSubmit={sendEmail}
       className="gap-5 text-red-600 flex flex-col items-center font-bold py-4 px-2 rounded-r border-red-600 border-2 border-solid w-3/4 sm:w-2/4"
     >
-      <label>Name</label>
+      <label htmlFor="Name">Name</label>
       <input
         type="text"
-        name="user_name"
+        name="Name"
         className="text-black font-normal w-full sm:w-2/4"
       />
-      <label>Email</label>
+      <label htmlFor="Email">Email</label>
       <input
         type="email"
-        name="user_email"
+        name="Email"
         className="text-black font-normal w-full sm:w-2/4"
       />
-      <label>Message</label>
-      <textarea
-        name="message"
+      <label htmlFor="Subject">Subject</label>
+      <input
+        type="text"
+        name="Subject"
         className="text-black font-normal w-full sm:w-2/4"
+      />
+      <label htmlFor="Message">Message</label>
+      <textarea
+        name="Message"
+        className="text-black font-normal w-full sm:w-2/4"
+        required={true}
       />
       <input
         type="submit"
